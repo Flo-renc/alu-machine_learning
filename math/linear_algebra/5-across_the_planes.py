b279b51995740a3654ff78ef83f7e05eb206447c
Flo-renc <f.kabeya@alustudent.com>
2025-05-24 17:00:38 +0200

    made pycodestyle changes to files

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -11,4 +11,4 @@ def add_matrices2D(mat1, mat2):
     if any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
         return None
     return [[a + b for a, b in zip(row1, row2)]
-    for row1, row2 in zip(mat1, mat2)]
+        for row1, row2 in zip(mat1, mat2)]
