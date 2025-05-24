5662a6e8d0915545e0c866dfe645f75923e3fe54
Flo-renc <f.kabeya@alustudent.com>
2025-05-24 17:07:41 +0200

    made pycodestyle changes to files

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -3,14 +3,10 @@
 Performs element-wise operations on matrices using NumPy
 """
 
-import numpy as np
 
 def np_elementwise(mat1, mat2):
     """
     Performs element-wise addition, subtraction, multiplication, and division.
-
-    Returns:
-        A tuple of (add, subtract, multiply, divide) results.
     """
-    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
-
+    return (mat1 + mat2, mat1 - mat2, 
+            mat1 * mat2, mat1 / mat2)
