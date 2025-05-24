55b382971a3c0d930b71364be06c4c9a91f29609
Flo-renc <f.kabeya@alustudent.com>
2025-05-24 16:33:07 +0200

    made pycodestyle changes to files

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -7,7 +7,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Concatenates two 2D matrices along the specified axis.
     """
     if axis == 0:
-        if (len(mat1[0]) 
+        if (len(mat1[0])
             != len(mat2[0])):
             return None
         return [row[:] for row in mat1] + [row[:] for row in mat2]
