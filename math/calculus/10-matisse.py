dac5d1227fcd9fe065da72eab4e85eb7193c937b
Flo-renc <f.kabeya@alustudent.com>
2025-06-14 01:45:03 +0200

    tasks on calculus completed

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -9,13 +9,6 @@ def poly_derivative(poly):
     """
     Calculates the derivative of a polynomial.
 
-    Args:
-        poly (list): A list of coefficients. The index is the power of x.
-
-    Returns:
-        list: A list of coefficients representing the 
-        derivative, or [0] if constant,
-        or None if input is invalid.
     """
     if not isinstance(poly, list) or len(poly) == 0:
         return None
